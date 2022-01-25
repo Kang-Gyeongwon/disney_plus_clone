b5ae4226c57e349705f35c3fb18c6fecd03ed6f4
Kang-Gyeongwon <rosie02644@gmail.com>
2022-01-25 23:40:34 +0900

    feat: 페이지 url 설정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/home"
 import LogInPage from "./pages/login"
 import MainPage from "./pages/main"
-import SingUpPage from "./pages/signup"
+import SearchPage from "./pages/search"
+import SelectProfilePage from "./pages/selectProfile"
+import SignUpPage from "./pages/signup"
 
 const App = () => {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/" element={<LogInPage />} />
-      <Route path="/" element={<SingUpPage />} />
+      <Route path="/login" element={<LogInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route path="/select-profile" element={<SelectProfilePage />} />
+      <Route path="/search" element={<SearchPage />} />
     </Routes>
   </BrowserRouter>
 }
