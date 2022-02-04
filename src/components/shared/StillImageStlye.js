20c5b805e30ac65b8ced04c2225c12d5f5ae0dea
Kang-Gyeongwon <rosie02644@gmail.com>
2022-02-05 00:41:17 +0900

    chore: StillImage 스타일 수정

--- src/components/shared/StillImageStlye.js
+++ src/components/shared/StillImageStlye.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StillImageBox = styled.div`
   width: 23.481%; height: 86.718%;
   box-sizing: border-box; overflow: hidden;
+  margin: 0; padding: 0;
 `
 
 export const StillImg = styled.img`
