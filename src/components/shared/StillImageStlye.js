9e421a3354523268f4a0439a4eead394c8724971
Kang-Gyeongwon <rosie02644@gmail.com>
2022-02-05 00:36:39 +0900

    feat: StillImage 공통 스타일 작성

--- src/components/shared/StillImageStlye.js
+++ src/components/shared/StillImageStlye.js
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+
+export const StillImageBox = styled.div`
+  width: 23.481%; height: 86.718%;
+  box-sizing: border-box; overflow: hidden;
+`
+
+export const StillImg = styled.img`
+  max-width: 100%; height: auto;
+  background-repeat: no-repeat; background-position: center; background-size: 100% 100%;
+`
