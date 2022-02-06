8ca63cbf75829a295df670a047e243c2dcbabfcb
Kang-Gyeongwon <rosie02644@gmail.com>
2022-02-06 22:01:20 +0900

    chore: StillImage.js파일 수정 및 StillImageStyle.js파일 삭제

--- src/components/shared/StillImage.js
+++ src/components/shared/StillImage.js
@@ -1,4 +1,15 @@
-import { StillImageBox, StillImg } from "./StillImageStlye"
+import styled from "styled-components";
+
+const StillImageBox = styled.div`
+  width: 23.481%; height: 86.718%;
+  box-sizing: border-box; overflow: hidden;
+  margin: 0; padding: 0;
+`
+
+const StillImg = styled.img`
+  max-width: 100%; height: auto;
+  background-repeat: no-repeat; background-position: center; background-size: 100% 100%;
+`
 
 const StillImage = () => {
   return <StillImageBox>
