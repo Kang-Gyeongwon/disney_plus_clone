ca10b52f15cc06b5884ae7bbbd01b77f3911d918
Kang-Gyeongwon <rosie02644@gmail.com>
2022-02-07 20:31:27 +0900

    chore: StillImage.js 파일 수정

--- src/components/shared/StillImage.js
+++ src/components/shared/StillImage.js
@@ -13,7 +13,7 @@ const StillImg = styled.img`
 
 const StillImage = () => {
   return <StillImageBox>
-    <StillImg src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/84A597261952771566AA11482D89DB0472703DFF4322A39DA9EE1D2A8B993E41/scale?width=2880&aspectRatio=1.78&format=jpeg" />
+    <StillImg src="" />
   </StillImageBox>
 }
 
